a04d164b55b15e4549301d4260efc4830307f0bf
Sidremus <remus.sid@gmail.com>
2023-11-20 21:30:32 +0100

    readme
    better video
    
    added readme
    
    Create torch_thumbnail_video.mp4
    
    updated readme
    
    embedding test
    
    Update README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# VFX Tutorial Project for Godot 4
+
+This project is intended to showcase how to build VFX in the Godot game engine (currently 4.1).
+
+I'm in the process of editing a video for this first low-poly fire effect. Once that's up I will, over time, add more effect types and showcase how they can be built in Godot.
+
+https://github.com/Sidremus/Godot-4.1-VFX-Tutorial-Project/assets/62385452/3adb5e54-7dcd-4416-87f7-d90a92729fcc
--- torch VFX/torchlight.gd
+++ torch VFX/torchlight.gd
@@ -1,16 +1,22 @@
 extends OmniLight3D
 
 @export var noise:Noise
-@onready var ref_pos: Vector3 = position
-@onready var audio_stream_player_3d: AudioStreamPlayer3D = $"../AudioStreamPlayer3D"
-
-var t:float
 @export var radius:float = .55
 
+@onready var original_position: Vector3 = position
+var offset: Vector3
+var time: float
+
+@export var audio_stream_player_3d: AudioStreamPlayer3D
+
 func _physics_process(delta: float) -> void:
-	t += delta
-	position = ref_pos + radius * Vector3(noise.get_noise_3d(t,0,0),noise.get_noise_3d(0,t,0),noise.get_noise_3d(0,0,t))
-	if t > 100: t = t - 100
+	time += delta
+	
+	offset.x = noise.get_noise_3d(time,0,0)
+	offset.y = noise.get_noise_3d(0,time,0)
+	offset.z = noise.get_noise_3d(0,0,time)
+	
+	position = original_position + radius * offset
 
 func _ready() -> void:
 	audio_stream_player_3d.seek(randf_range(0., audio_stream_player_3d.stream.get_length()))
